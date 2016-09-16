2d025fd28ea6e8c57eb93f46526f71eed5324526
左旭 <Mr.Xu@zuoxudeMacBook-Pro.local>
2016-09-16 14:49:26 +0800

    小乔转

---
+++ QuickCure/QuickCure/Classes/Controllers/ZXDoctor.h
@@ -0,0 +1,13 @@
+//
+//  ZXDoctor.h
+//  QuickCure
+//
+//  Created by 左旭 on 16/9/16.
+//  Copyright © 2016年 haoge. All rights reserved.
+//
+
+#import "BaseNaviController.h"
+
+@interface ZXDoctor : BaseNaviController
+
+@end
